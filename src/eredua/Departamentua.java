b61365d97ad12d7788be88f67976b3cf22c4f621
rocio <rocio@rocio-VirtualBox>
2019-10-18 15:53:55 +0200

    Langilea eta departamentu klasea sortu.

---
+++ src/eredua/Departamentua.java
@@ -0,0 +1,63 @@
+package eredua;
+
+public class Departamentua {
+	private String depart_kod, izena, kokapena, eraikuntza_zbk, irakasle_kop;
+	
+	//konstruktorea
+	public Departamentua(String depart_kod, String izena, String kokapena, String eraikuntza_zbk, String irakasle_kop) {
+		this.depart_kod = depart_kod;
+		this.izena = izena;
+		this.kokapena = kokapena;
+		this.eraikuntza_zbk = eraikuntza_zbk;
+		this.irakasle_kop = irakasle_kop;
+	}
+	
+	//getters and setters
+	public String getDepart_kod() {
+		return depart_kod;
+	}
+
+	public void setDepart_kod(String depart_kod) {
+		this.depart_kod = depart_kod;
+	}
+
+	public String getIzena() {
+		return izena;
+	}
+
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+
+	public String getKokapena() {
+		return kokapena;
+	}
+
+	public void setKokapena(String kokapena) {
+		this.kokapena = kokapena;
+	}
+
+	public String getEraikuntza_zbk() {
+		return eraikuntza_zbk;
+	}
+
+	public void setEraikuntza_zbk(String eraikuntza_zbk) {
+		this.eraikuntza_zbk = eraikuntza_zbk;
+	}
+
+	public String getIrakasle_kop() {
+		return irakasle_kop;
+	}
+
+	public void setIrakasle_kop(String irakasle_kop) {
+		this.irakasle_kop = irakasle_kop;
+	}
+
+	//tostring
+	@Override
+	public String toString() {
+		return "Departamentua [depart_kod=" + depart_kod + ", izena=" + izena + ", kokapena=" + kokapena
+				+ ", eraikuntza_zbk=" + eraikuntza_zbk + ", irakasle_kop=" + irakasle_kop + "]";
+	}
+
+}
---
+++ src/eredua/Langilea.java
@@ -0,0 +1,73 @@
+package eredua;
+
+public class Langilea {
+	private String nan, izena, abizenak, ardura, arduraduna, departamentu_kod;
+	//konstruktorea
+
+	public Langilea(String nan, String izena, String abizenak, String ardura, String arduraduna,
+			String departamentu_kod) {
+		this.nan = nan;
+		this.izena = izena;
+		this.abizenak = abizenak;
+		this.ardura = ardura;
+		this.arduraduna = arduraduna;
+		this.departamentu_kod = departamentu_kod;
+	}
+	
+	//getters and setters
+	public String getNan() {
+		return nan;
+	}
+
+	public void setNan(String nan) {
+		this.nan = nan;
+	}
+
+	public String getIzena() {
+		return izena;
+	}
+
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+
+	public String getAbizenak() {
+		return abizenak;
+	}
+
+	public void setAbizenak(String abizenak) {
+		this.abizenak = abizenak;
+	}
+
+	public String getArdura() {
+		return ardura;
+	}
+
+	public void setArdura(String ardura) {
+		this.ardura = ardura;
+	}
+
+	public String getArduraduna() {
+		return arduraduna;
+	}
+
+	public void setArduraduna(String arduraduna) {
+		this.arduraduna = arduraduna;
+	}
+
+	public String getDepartamentu_kod() {
+		return departamentu_kod;
+	}
+
+	public void setDepartamentu_kod(String departamentu_kod) {
+		this.departamentu_kod = departamentu_kod;
+	}
+
+	//tostring
+	@Override
+	public String toString() {
+		return "Langilea [nan=" + nan + ", izena=" + izena + ", abizenak=" + abizenak + ", ardura=" + ardura
+				+ ", arduraduna=" + arduraduna + ", departamentu_kod=" + departamentu_kod + "]";
+	}
+	
+}
