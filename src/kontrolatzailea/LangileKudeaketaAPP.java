27058d149fed6d9fe398372746cb815f3265e307
rocio <rocio@rocio-VirtualBox>
2019-10-19 13:11:57 +0200

    Lehenengo leihoa ireki.

--- src/kontrolatzailea/LangileKudeaketaAPP.java
+++ src/kontrolatzailea/LangileKudeaketaAPP.java
@@ -1,11 +1,13 @@
 package kontrolatzailea;
+
+import kontrolatzailea.MetodoakLeihoAldaketa;
 import eredua.*;
 
 public class LangileKudeaketaAPP {
 
 	public static void main(String[] args) {
-
 		Konexioa konexioa = new Konexioa();
+		MetodoakLeihoAldaketa.lehenengoLeihoa();
 	}
 
 }
