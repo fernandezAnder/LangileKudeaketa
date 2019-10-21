07d62e5d5939974ebcf8f77db0b442c56bbd8cc2
Admin1 <39264938+goizanet@users.noreply.github.com>
2019-10-21 10:46:21 +0200

    keylistener

--- src/ikuspegia/Leiho3EnpleKudeaketa.java
+++ src/ikuspegia/Leiho3EnpleKudeaketa.java
@@ -1,13 +1,19 @@
 package ikuspegia;
 
 import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Leiho3EnpleKudeaketa extends JFrame {
 	
 	
 	private static final long serialVersionUID = 1L;
+	private JTextField txtSaberBorrar;
+	private char letra;
+
 	/**
 	 * Ongietorria ematen duen panela sortu
 	 * @author talde3
@@ -17,10 +23,28 @@ public class Leiho3EnpleKudeaketa extends JFrame {
 		//panelaren propietateak
 //		setIconImage(Toolkit.getDefaultToolkit().getImage(".\\Argazkiak\\logoa.png")); 
 		getContentPane().setLayout(null);
+		
 		this.setBounds(350,50,600,600);
 		this.setTitle("6.taldearen langileen kudeaketa");
 		this.setResizable(false); // neurketak ez aldatzeko
 		this.setSize(new Dimension(600, 600));
+		
+		
+		txtSaberBorrar = new JTextField();
+		txtSaberBorrar.setText("SABER BORRAR");
+		txtSaberBorrar.setBounds(96, 56, 86, 20);
+		getContentPane().add(txtSaberBorrar);
+		txtSaberBorrar.addKeyListener(new KeyAdapter() {
+			public void keyTyped(KeyEvent e) {
+				letra = e.getKeyChar();
+				// bakarrik karakter bat (V,v,M,m) sartzekoa
+				if (txtSaberBorrar.getText().length() > 5 || letra != 'E' && letra != 'e' && letra != 'G' && letra != 'g')
+					e.consume(); // ez du godetzen
+			}
+		});
+		
+		getContentPane().add(txtSaberBorrar);
+		txtSaberBorrar.setColumns(10);
 		
 	}		
 }
