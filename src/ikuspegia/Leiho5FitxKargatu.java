89017f2679765c86e14e5ef337b643559699ee94
Ander Fernandez <ander.fernandezla@elorrieta-errekamari.com>
2019-10-28 10:42:39 +0100

    A

--- src/ikuspegia/Leiho5FitxKargatu.java
+++ src/ikuspegia/Leiho5FitxKargatu.java
@@ -15,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
+import javax.swing.DefaultComboBoxModel;
 
 public class Leiho5FitxKargatu extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -29,7 +30,7 @@ public class Leiho5FitxKargatu extends JFrame {
 		this.setBounds(350,50,600,600);
 		this.setTitle("6.taldearen langileen kudeaketa");
 		this.setResizable(false); // neurketak ez aldatzeko
-		this.setSize(new Dimension(600, 600));
+		this.setSize(new Dimension(606, 252));
 		
 		
 		lblMenuNagusia = new JLabel("Fitxategia Kargatu\r\n");
@@ -38,32 +39,69 @@ public class Leiho5FitxKargatu extends JFrame {
 		getContentPane().add(lblMenuNagusia);
 		
 		btnIrten = new JButton("Irten");
-		btnIrten.setBounds(447, 510, 89, 23);
+		btnIrten.setBounds(333, 183, 89, 23);
 		getContentPane().add(btnIrten);
 		
 		lblMota = new JLabel("Mota: ");
 		lblMota.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblMota.setBounds(35, 127, 55, 23);
+		lblMota.setBounds(32, 82, 55, 23);
 		getContentPane().add(lblMota);
 		
 		comboBox = new JComboBox();
-		comboBox.setFont(new Font("Dialog", Font.BOLD, 18));
-		comboBox.setBounds(100, 130, 98, 20);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Departamentua", "Langilea"}));
+		comboBox.setFont(new Font("Dialog", Font.PLAIN, 13));
+		comboBox.setBounds(97, 85, 134, 20);
 		getContentPane().add(comboBox);
 		
 		lblRuta = new JLabel("Ruta: ");
 		lblRuta.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblRuta.setBounds(35, 180, 55, 23);
+		lblRuta.setBounds(32, 135, 55, 23);
 		getContentPane().add(lblRuta);
 		
 		textField = new JTextField();
 		textField.setFont(new Font("Dialog", Font.BOLD, 18));
-		textField.setBounds(98, 183, 407, 20);
+		textField.setBounds(95, 138, 407, 20);
 		getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		btnKargatu = new JButton("Kargatu");
-		btnKargatu.setBounds(294, 510, 89, 23);
+		btnKargatu.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String helbidea = textField.getText();
+				String[] banaketa = helbidea.split(".");
+				if (comboBox.getSelectedItem().equals("Departamentua")) {
+					
+					if (banaketa[1].equals("csv")) {
+						
+					}else if(banaketa[1].equals("xml")) {
+						
+					}else if(banaketa[1].equals("json")) {
+						
+					}else {
+						System.out.println("Errorea formatua aukeratzean");
+					}
+					
+				}
+				else if (comboBox.getSelectedItem().equals("Langilea")) {
+					
+					if (banaketa[1].equals("csv")) {
+						System.out.println("ONDO CSV");
+					}else if(banaketa[1].equals("xml")) {
+						System.out.println("ONDO XML");
+						
+					}else if(banaketa[1].equals("json")) {
+						System.out.println("ONDO JSON");
+					}else {
+						System.out.println("Errorea formatua aukeratzean");
+					}
+					
+					
+				}else {
+					System.out.println("Ez da mota hautatu");
+				}
+			}
+		});
+		btnKargatu.setBounds(451, 183, 89, 23);
 		getContentPane().add(btnKargatu);
 		
 		button = new JButton("...");
@@ -78,7 +116,7 @@ public class Leiho5FitxKargatu extends JFrame {
 				}
 			}
 		});
-		button.setBounds(515, 182, 28, 23);
+		button.setBounds(512, 137, 28, 23);
 		getContentPane().add(button);
 		
 		
